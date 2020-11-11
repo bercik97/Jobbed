3c993e0a0f750d8c2c35dfb223e76ff1eb7c4d74
RobertKrzywina <robert.krzywina@gmail.com>
2020-11-11 17:50:12 +0100

    Add missing icons at employee panel + add new btn for work timer

--- lib/employee/profile/tabs/employee_panel.dart
+++ lib/employee/profile/tabs/employee_panel.dart
@@ -6,7 +6,6 @@ import 'package:give_job/internationalization/localization/localization_constant
 import 'package:give_job/shared/libraries/colors.dart';
 import 'package:give_job/shared/model/user.dart';
 import 'package:give_job/shared/service/toastr_service.dart';
-import 'package:give_job/shared/widget/icons.dart';
 import 'package:give_job/shared/widget/texts.dart';
 
 import 'contact/contact_for_manager.dart';
@@ -16,41 +15,62 @@ Container employeePanel(BuildContext context, User user, EmployeePageDto employe
     child: Padding(
       padding: const EdgeInsets.all(12.0),
       child: SingleChildScrollView(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Material(
-                color: BRIGHTER_DARK,
-                child: InkWell(
-                  onTap: () => Navigator.of(context).push(
-                    CupertinoPageRoute<Null>(
-                      builder: (BuildContext context) {
-                        return EmployeeCalendarPage(user, employee.id);
-                      },
+        child: Column(
+          children: [
+            SizedBox(width: 10),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Material(
+                    color: BRIGHTER_DARK,
+                    child: InkWell(
+                      onTap: () => {},
+                      child: _buildScrollableContainer(context, 'images/big-employee-work-icon.png', 'workingTime', 'startFinishWork'),
                     ),
                   ),
-                  child: _buildScrollableContainer(context, Icons.today, 'calendar', 'checkYourCalendar'),
                 ),
-              ),
-            ),
-            SizedBox(width: 10),
-            Expanded(
-              child: Material(
-                color: BRIGHTER_DARK,
-                child: InkWell(
-                  onTap: () => employee.groupManager != null
-                      ? showContactForManager(
-                          context,
-                          employee.groupManager,
-                          employee.groupManagerPhone,
-                          employee.groupManagerViber,
-                          employee.groupManagerWhatsApp,
-                        )
-                      : ToastService.showErrorToast(getTranslated(context, 'noManagerAssigned')),
-                  child: _buildScrollableContainer(context, Icons.phone, 'contact', 'contactWithYourManager'),
+                SizedBox(width: 10),
+                Expanded(
+                  child: Material(
+                    color: BRIGHTER_DARK,
+                    child: InkWell(
+                      onTap: () => employee.groupManager != null
+                          ? showContactForManager(
+                              context,
+                              employee.groupManager,
+                              employee.groupManagerPhone,
+                              employee.groupManagerViber,
+                              employee.groupManagerWhatsApp,
+                            )
+                          : ToastService.showErrorToast(getTranslated(context, 'noManagerAssigned')),
+                      child: _buildScrollableContainer(context, 'images/big-contact-with-manager-icon.png', 'contact', 'contactWithYourManager'),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: Material(
+                    color: BRIGHTER_DARK,
+                    child: InkWell(
+                      onTap: () => Navigator.of(context).push(
+                        CupertinoPageRoute<Null>(
+                          builder: (BuildContext context) {
+                            return EmployeeCalendarPage(user, employee.id);
+                          },
+                        ),
+                      ),
+                      child: _buildScrollableContainer(context, 'images/big-documents-icon.png', 'calendar', 'checkYourCalendar'),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(child: Material(color: BRIGHTER_DARK)),
+              ],
+            )
           ],
         ),
       ),
@@ -58,13 +78,21 @@ Container employeePanel(BuildContext context, User user, EmployeePageDto employe
   );
 }
 
-Widget _buildScrollableContainer(BuildContext context, IconData icon, String title, String subtitle) {
+Widget _buildScrollableContainer(BuildContext context, String imagePath, String title, String subtitle) {
   return Container(
-    height: 120,
+    height: 160,
     child: SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: Column(
-        children: <Widget>[SizedBox(height: 10), icon50Green(icon), text18WhiteBold(getTranslated(context, title)), Padding(padding: EdgeInsets.only(left: 10, right: 10), child: textCenter13White(getTranslated(context, subtitle))), SizedBox(height: 10)],
+        children: <Widget>[
+          Image(height: 100, image: AssetImage(imagePath)),
+          text18WhiteBold(getTranslated(context, title)),
+          Padding(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: textCenter13White(getTranslated(context, subtitle)),
+          ),
+          SizedBox(height: 10),
+        ],
       ),
     ),
   );
