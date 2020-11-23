bd674c8b91a20701182885c563923d3d1869f06e
RobertKrzywina <robert.krzywina@gmail.com>
2020-11-23 15:10:16 +0100

    Delete unused update method

--- lib/api/warehouse/service/warehouse_service.dart
+++ lib/api/warehouse/service/warehouse_service.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:give_job/api/warehouse/dto/create_warehouse_dto.dart';
-import 'package:give_job/api/warehouse/dto/update_warehouse_dto.dart';
 import 'package:give_job/api/warehouse/dto/warehouse_dashboard_dto.dart';
-import 'package:give_job/api/warehouse/dto/warehouse_dto.dart';
 import 'package:give_job/shared/libraries/constants.dart';
 import 'package:give_job/shared/service/logout_service.dart';
 import 'package:http/http.dart';
@@ -40,22 +38,6 @@ class WarehouseService {
     );
     if (res.statusCode == 200) {
       return (json.decode(res.body) as List).map((data) => WarehouseDashboardDto.fromJson(data)).toList();
-    } else if (res.statusCode == 401) {
-      return Logout.handle401WithLogout(_context);
-    } else {
-      return Future.error(res.body);
-    }
-  }
-
-  Future<dynamic> update(int id, UpdateWarehouseDto dto) async {
-    String url = '$_url/$id';
-    Response res = await put(
-      url,
-      body: jsonEncode(UpdateWarehouseDto.jsonEncode(dto)),
-      headers: _headers,
-    );
-    if (res.statusCode == 200) {
-      return res;
     } else if (res.statusCode == 401) {
       return Logout.handle401WithLogout(_context);
     } else {
