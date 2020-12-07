7d8c3d4a9650df77e7038c0a5b733d9b49e05327
RobertKrzywina <robert.krzywina@gmail.com>
2020-12-08 00:03:41 +0100

    Modify add piecework page

--- lib/employee/profile/tabs/piecework/add/add_piecework_page.dart
+++ lib/employee/profile/tabs/piecework/add/add_piecework_page.dart
@@ -20,11 +20,11 @@ import 'package:give_job/shared/libraries/constants.dart';
 import 'package:give_job/shared/model/user.dart';
 import 'package:give_job/shared/service/dialog_service.dart';
 import 'package:give_job/shared/service/toastr_service.dart';
-import 'package:give_job/shared/service/validator_service.dart';
 import 'package:give_job/shared/util/navigator_util.dart';
 import 'package:give_job/shared/widget/icons.dart';
 import 'package:give_job/shared/widget/loader.dart';
 import 'package:give_job/shared/widget/texts.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 
 import '../piecework_page.dart';
 
@@ -52,9 +52,9 @@ class _AddPieceworkPageState extends State<AddPieceworkPage> {
   final ScrollController _scrollController = new ScrollController();
 
   final TextEditingController _workplaceNameController = new TextEditingController();
+  final Map<String, TextEditingController> _textEditingItemControllers = new Map();
 
   List<PricelistDto> _pricelists = new List();
-  List<PricelistDto> _filteredPricelists = new List();
 
   Map<String, int> serviceWithQuantity = new LinkedHashMap();
 
@@ -73,7 +73,7 @@ class _AddPieceworkPageState extends State<AddPieceworkPage> {
     _pricelistService.findAllByCompanyId(int.parse(_user.companyId)).then((res) {
       setState(() {
         _pricelists = res;
-        _filteredPricelists = _pricelists;
+        _pricelists.forEach((i) => _textEditingItemControllers[utf8.decode(i.name.runes.toList())] = new TextEditingController());
         _loading = false;
       });
     }).catchError((onError) {
@@ -153,10 +153,6 @@ class _AddPieceworkPageState extends State<AddPieceworkPage> {
                     getTranslated(context, 'workplaceNameIsRequired'),
                   ),
                   SizedBox(height: 10),
-                  _buildAddedPriceList(),
-                  SizedBox(height: 20),
-                  _buildLoupe(),
-                  SizedBox(height: 10),
                   _buildPricelist(),
                 ],
               ),
@@ -174,101 +170,27 @@ class _AddPieceworkPageState extends State<AddPieceworkPage> {
   }
 
   Widget _buildField(TextEditingController controller, String hintText, String labelText, int length, int lines, String errorText) {
-    return TextFormField(
-      autofocus: false,
-      controller: controller,
-      autocorrect: true,
-      keyboardType: TextInputType.multiline,
-      maxLength: length,
-      maxLines: lines,
-      cursorColor: WHITE,
-      textAlignVertical: TextAlignVertical.center,
-      style: TextStyle(color: WHITE),
-      validator: RequiredValidator(errorText: errorText),
-      decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: WHITE, width: 2)),
-        counterStyle: TextStyle(color: WHITE),
-        border: OutlineInputBorder(),
-        hintText: hintText,
-        labelText: labelText,
-        labelStyle: TextStyle(color: WHITE),
-      ),
-    );
-  }
-
-  Widget _buildLoupe() {
     return Container(
       padding: EdgeInsets.only(left: 10, right: 10),
       child: TextFormField(
         autofocus: false,
+        controller: controller,
         autocorrect: true,
+        keyboardType: TextInputType.multiline,
+        maxLength: length,
+        maxLines: lines,
         cursorColor: WHITE,
+        textAlignVertical: TextAlignVertical.center,
         style: TextStyle(color: WHITE),
+        validator: RequiredValidator(errorText: errorText),
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: WHITE, width: 2)),
           counterStyle: TextStyle(color: WHITE),
           border: OutlineInputBorder(),
-          labelText: getTranslated(this.context, 'search'),
-          prefixIcon: iconWhite(Icons.search),
+          hintText: hintText,
+          labelText: labelText,
           labelStyle: TextStyle(color: WHITE),
         ),
-        onChanged: (string) {
-          setState(
-            () {
-              _filteredPricelists = _pricelists.where((e) => ((e.name + e.price.toString()).toLowerCase().contains(string.toLowerCase()))).toList();
-            },
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildAddedPriceList() {
-    if (serviceWithQuantity.isEmpty) {
-      return textCenter18Green(getTranslated(context, 'noAddedItemsFromPricelist'));
-    }
-    Iterable keys = serviceWithQuantity.keys;
-    Iterable values = serviceWithQuantity.values;
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Theme(
-                data: Theme.of(context).copyWith(dividerColor: MORE_BRIGHTER_DARK),
-                child: DataTable(
-                  columnSpacing: 10,
-                  columns: [
-                    DataColumn(label: textWhiteBold('No.')),
-                    DataColumn(label: textWhiteBold(getTranslated(context, 'serviceName'))),
-                    DataColumn(label: textWhiteBold(getTranslated(context, 'quantity'))),
-                    DataColumn(label: textWhiteBold(getTranslated(context, 'remove'))),
-                  ],
-                  rows: [
-                    for (int i = 0; i < keys.length; i++)
-                      DataRow(
-                        cells: [
-                          DataCell(textWhite((i + 1).toString())),
-                          DataCell(textWhite(keys.elementAt(i))),
-                          DataCell(textWhite(values.elementAt(i).toString())),
-                          DataCell(
-                            MaterialButton(
-                              child: iconWhite(Icons.close),
-                              color: Colors.red,
-                              onPressed: () => setState(() => serviceWithQuantity.remove(keys.elementAt(i))),
-                            ),
-                          ),
-                        ],
-                      ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
@@ -281,41 +203,34 @@ class _AddPieceworkPageState extends State<AddPieceworkPage> {
         controller: _scrollController,
         child: ListView.builder(
           controller: _scrollController,
-          itemCount: _filteredPricelists.length,
+          itemCount: _pricelists.length,
           itemBuilder: (BuildContext context, int index) {
-            PricelistDto pricelist = _filteredPricelists[index];
+            PricelistDto pricelist = _pricelists[index];
             String name = utf8.decode(pricelist.name.runes.toList());
             String price = pricelist.price.toString();
+            TextEditingController controller = _textEditingItemControllers[name];
             return Card(
               color: DARK,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Container(
+                  Card(
                     color: BRIGHTER_DARK,
                     child: ListTile(
-                      trailing: InkWell(
-                        child: IconButton(
-                          icon: icon30Green(Icons.add),
-                          onPressed: () {
-                            if (serviceWithQuantity.containsKey(name)) {
-                              ToastService.showErrorToast(getTranslated(this.context, 'youHaveAlreadyChosenThisPricelistService'));
-                              return;
-                            }
-                            _setPricelistServiceQuantity(name);
-                          },
-                        ),
-                      ),
-                      title: text20WhiteBold(name),
+                      title: textGreen(name),
                       subtitle: Row(
                         children: [
                           textWhite(getTranslated(this.context, 'price') + ': '),
                           textGreen(price),
                         ],
                       ),
+                      trailing: Container(
+                        width: 100,
+                        child: _buildNumberField(controller),
+                      ),
                     ),
-                  )
+                  ),
                 ],
               ),
             );
@@ -325,106 +240,12 @@ class _AddPieceworkPageState extends State<AddPieceworkPage> {
     );
   }
 
-  void _setPricelistServiceQuantity(String pricelistService) {
-    TextEditingController _quantityController = new TextEditingController();
-    showGeneralDialog(
-      context: context,
-      barrierColor: DARK.withOpacity(0.95),
-      barrierDismissible: false,
-      barrierLabel: getTranslated(context, 'quantity'),
-      transitionDuration: Duration(milliseconds: 400),
-      pageBuilder: (_, __, ___) {
-        return SizedBox.expand(
-          child: Scaffold(
-            backgroundColor: Colors.black12,
-            body: Center(
-              child: Padding(
-                padding: EdgeInsets.only(left: 10, right: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(top: 50),
-                      child: Column(
-                        children: [
-                          text20GreenBold(pricelistService),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 7.5),
-                    textGreen(getTranslated(context, 'typeQuantityOfServicePerformed')),
-                    SizedBox(height: 2.5),
-                    Container(
-                      width: 150,
-                      child: TextFormField(
-                        autofocus: true,
-                        controller: _quantityController,
-                        keyboardType: TextInputType.numberWithOptions(decimal: false),
-                        maxLength: 3,
-                        cursorColor: WHITE,
-                        textAlignVertical: TextAlignVertical.center,
-                        style: TextStyle(color: WHITE),
-                        decoration: InputDecoration(
-                          counterStyle: TextStyle(color: WHITE),
-                          labelStyle: TextStyle(color: WHITE),
-                          labelText: '(1-999)',
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        MaterialButton(
-                          elevation: 0,
-                          height: 50,
-                          minWidth: 40,
-                          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[iconWhite(Icons.close)],
-                          ),
-                          color: Colors.red,
-                          onPressed: () => Navigator.pop(context),
-                        ),
-                        SizedBox(width: 25),
-                        MaterialButton(
-                          elevation: 0,
-                          height: 50,
-                          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              iconWhite(Icons.check),
-                            ],
-                          ),
-                          color: GREEN,
-                          onPressed: () {
-                            int quantity;
-                            try {
-                              quantity = int.parse(_quantityController.text);
-                            } catch (FormatException) {
-                              ToastService.showErrorToast(getTranslated(context, 'quantityIsRequired'));
-                              return;
-                            }
-                            String invalidMessage = ValidatorService.validatePricelistServiceQuantity(quantity, context);
-                            if (invalidMessage != null) {
-                              ToastService.showErrorToast(invalidMessage);
-                              return;
-                            }
-                            setState(() => serviceWithQuantity[pricelistService] = quantity);
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      },
+  _buildNumberField(TextEditingController controller) {
+    return NumberInputWithIncrementDecrement(
+      controller: controller,
+      min: 0,
+      style: TextStyle(color: GREEN),
+      widgetContainerDecoration: BoxDecoration(border: Border.all(color: BRIGHTER_DARK)),
     );
   }
 
@@ -476,6 +297,12 @@ class _AddPieceworkPageState extends State<AddPieceworkPage> {
       setState(() => _isAddButtonTapped = false);
       return;
     }
+    _textEditingItemControllers.forEach((name, quantityController) {
+      String quantity = quantityController.text;
+      if (quantity != '0') {
+        serviceWithQuantity[name] = int.parse(quantity);
+      }
+    });
     if (serviceWithQuantity.isEmpty) {
       DialogService.showCustomDialog(
         context: context,
