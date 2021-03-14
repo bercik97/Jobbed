feffc71903375d1a8a43b21f0418a5ad9f2f7ea0
Robert97 <robert.krzywina@gmail.com>
2021-03-14 23:25:30 +0100

    Change type of fetch data in add piecework for selected employees page

--- lib/manager/groups/group/piecework/manage/add_piecework_for_selected_employees_page.dart
+++ lib/manager/groups/group/piecework/manage/add_piecework_for_selected_employees_page.dart
@@ -97,6 +97,20 @@ class _AddPieceworkForSelectedEmployeesPageState extends State<AddPieceworkForSe
           padding: const EdgeInsets.all(12.0),
           child: Column(
             children: [
+              Padding(
+                padding: EdgeInsets.only(top: 15, left: 15, bottom: 10),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: text20Black(getTranslated(context, 'pieceworkForSelectedWorkdaysAndEmployees')),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5, left: 15, bottom: 10),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: text20BlueBold(_dateFrom + ' - ' + _dateTo),
+                ),
+              ),
               _loading ? circularProgressIndicator() : _buildPriceList(),
             ],
           ),
@@ -115,20 +129,6 @@ class _AddPieceworkForSelectedEmployeesPageState extends State<AddPieceworkForSe
           child: Center(
             child: Column(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 15, left: 15, bottom: 10),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: text20Black(getTranslated(context, 'pieceworkForSelectedWorkdaysAndEmployees')),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 5, left: 15, bottom: 10),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: text20BlueBold(_dateFrom + ' - ' + _dateTo),
-                  ),
-                ),
                 Column(
                   children: [
                     for (var priceList in _priceLists)
